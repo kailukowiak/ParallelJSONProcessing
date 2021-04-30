eae97d26af6efafcd1d482cc3b89d16b2db363bb
Kai Lukowiak <kailukowiak@gmail.com>
2021-04-30 19:40:29 +0000

    "Locally" Distributed

--- parser.jl
+++ parser.jl
@@ -1,35 +1,48 @@
 ## Libs
-using Revise
-using DataFrames
-using JSON
-using .Threads
-using CSV
-using ProgressMeter
-using AWSS3
-using AWS
-using FilePathsBase
+
 using Distributed
+nprocs = 4
+proc_ids = addprocs(nprocs)
+@everywhere import Pkg
+@everywhere Pkg.activate(".")
+@everywhere Pkg.instantiate()
+@everywhere begin
+
+    using Revise
+    using DataFrames
+    using Distributed
+    using JSON
+    using .Threads
+    using CSV
+    using ProgressMeter
+    using AWSS3
+    using AWS
+    using FilePathsBase
+    using ClusterManagers
+end
 ## AWS Setup
-aws = global_aws_config(; region="us-east-2")
-p = S3Path("s3://lukowiak-bucket/parsedata/",  config=global_aws_config())
+@everywhere aws = global_aws_config(; region="us-east-2")
+p = S3Path("s3://lukowiak-bucket/parsedata/",  config=aws)
 file_list = readdir(p)
 println(file_list)
+
 ##
-function download_execute(s3path, filename)
-    run(`aws s3 cp $s3path ./tmpdir/`)
-    json2csv("tmpdir/$filename", filename)
-    rm("tmpdir/$filename")
+@everywhere function download_execute(s3path)
+    filename = split(s3path, "/")[end]
+    run(`aws s3 cp $s3path ./tmpdir$(myid())/`)
+    json2csv("tmpdir$(myid())/$filename", filename)
+    rm("tmpdir$(myid())/$filename")
     
 end
 
 # file = joinpath(p, "file1.json")
 # s3_get_file(aws, "lukowiak-bucket", "parsedata", "file1.csv")
 ## parsing
-function json2csv(json_path, filename)
+@everywhere function json2csv(json_path, filename)
     file = open(json_path) do f
         split(read(f, String), "\n")
     end
-    df = DataFrame(first_name=String[], last_name=String[], occupation=String[])
+    df = DataFrame(first_name=String[], last_name=String[], occupation=String[], thread_id = Int[])
 
     df_list = Vector{DataFrame}()
     for t = 1:nthreads()
@@ -38,32 +51,25 @@ function json2csv(json_path, filename)
     @threads for line ∈ file[1:end - 1]
     
         json = JSON.parse(line)
+        json["thread_id"] = threadid()
         push!(df_list[threadid()], json)
-        # sleep(5)
+        sleep(5)
     end
 
     df = vcat(df_list...)
-    csv_name = filename # split(json_path, ".json")[1] * ".csv"
+    df[!, :proc_id] .= myid()
+    df[!, :computer] .= gethostname()
+    csv_name = split(filename, ".json")[1] * ".csv"  # split(json_path, ".json")[1] * ".csv"
     # CSV.write(csv_name, df)
     s3path = S3Path("s3://lukowiak-bucket/parsedata_csv/$csv_name",  config=global_aws_config())
-    # s3_put(aws, "lukowiak-bucket", "parsedata/$csv_name", df)
+    # s3_put(aws, "lukowiak-bucket", "parsedata/$csv_name", df) 
     CSV.write(s3path, df)
 end
 
-for i ∈ 1:length(file_list)
-    download_execute("s3://lukowiak-bucket/parsedata/$(file_list[i])", file_list[i])
-end
+download_execute("s3://lukowiak-bucket/parsedata/file1.json")
 
-json2csv("data/file1.json")
+# for file ∈ file_list
+#     download_execute("s3://lukowiak-bucket/parsedata/$file")
+# end
 
-
-test = open(file) do s
-    data = UInt8[]
-    readbytes!(s,data,Inf)
-end
-
-s = open(file,"rb")
-data = UInt8[]
-readbytes!(s,data,Inf)
-close(s)
-run(`aws s3 cp s3://lukowiak-bucket/parsedata/file3.json .`)
+pmap(download_execute, "s3://lukowiak-bucket/parsedata/" .* file_list)
